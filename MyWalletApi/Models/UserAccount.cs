9c8923bb931db5181bc6cac4cd9b9c3e23d4dbf9
Anthony Tompkins <anthonyitompkins@gmail.com>
2025-03-04 11:35:19 -0500

    UserAccount Email and Password were incorrectly created as INT.  Changed to STRING.

--- MyWalletApi/Models/UserAccount.cs
+++ MyWalletApi/Models/UserAccount.cs
@@ -10,10 +10,10 @@ namespace MyWalletApi.Models
 
         [Required(ErrorMessage = "Email is required")]
         [MaxLength(100, ErrorMessage ="Maximum email length is 100.")]
-        public int Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [MaxLength(25, ErrorMessage = "Maximum password length is 25.")]
-        public int Password { get; set; }
+        public required string Password { get; set; }
     }
 }
