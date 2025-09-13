ac44425bfe2a88c663427b699af7cffb12a49841
Anthony Tompkins <anthonyitompkins@gmail.com>
2025-09-13 08:02:16 -0400

    Removed connection from TompkinsContext class into appsettings.json

--- MyWalletApi/Models/TompkinsContext.cs
+++ MyWalletApi/Models/TompkinsContext.cs
@@ -33,10 +33,6 @@ public partial class TompkinsContext : DbContext
 
     public virtual DbSet<UserAccount> UserAccounts { get; set; }  
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:tompkinssqlserver.database.windows.net,1433;Initial Catalog=Tompkins;Persist Security Info=False;User ID=tompkinsadmin;Password=Binural.10.LightYears;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
